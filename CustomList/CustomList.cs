28ccb872cd4aa44391b0960bf63fcce52d10d4cf
Alton88 <altonyee@gmail.com>
2016-09-30 08:36:30 -0500

    First Commit

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomList
+{
+    public class CustomList<T> : IEnumerable<T> where T : IComparable
+    {
+        private T[] items;
+        private int capacity;
+        private int manyItems;
+
+        public CustomList()
+        {
+            capacity = 5;
+            items = new T[capacity];
+            manyItems = 0;
+        }
+        public int Count
+        {
+            get { return manyItems; }
+        }
+        public void Add(T element)
+        {
+            try { 
+                if (manyItems == items.Length) {
+                    EnsureCapacity(manyItems * 2 + 1);
+                }
+                this.items[manyItems] = element;
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+            ++manyItems;
+        }
+        public void EnsureCapacity(int minimumCapacity)
+        {
+            T[] biggerArray;
+
+            if (items.Length < minimumCapacity)
+            {
+                biggerArray = new T[minimumCapacity];
+                try { 
+                    for (int i = 0; i < manyItems; ++i)
+                    {
+                        biggerArray[i] = items[i];
+                    }
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+
+                items = biggerArray;
+            }
+        }
+        public bool Remove(T target)
+        {
+            int index = -1;
+
+            for (int i = 0; i < manyItems; ++i)
+            {
+                if (items[i].Equals(target)) { index = i; }
+            }
+
+            if (index >= 0)
+            {
+                --manyItems;
+
+                try { 
+                    for (int i = 0; i < manyItems; ++i) {
+                        if (i >= index) { items[i] = items[i + 1]; }
+                    }
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+
+                return true;
+            }
+            else { return false; }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < manyItems; ++i) {
+                yield return items[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            string output = null;
+            
+            try { 
+                for (int i = 0; i < manyItems; ++i)
+                {   
+                     output += items[i] + " ";         
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
+            return output;
+        }
+        public static CustomList<T> operator +(CustomList<T> listOne, CustomList<T> listTwo)
+        {
+            CustomList<T> temporaryList = new CustomList<T>();
+            foreach (T element in listOne)
+            {
+                temporaryList.Add(element);
+            }
+            foreach (T element in listTwo)
+            {
+                temporaryList.Add(element);
+            }
+            return temporaryList;
+        }
+        public static CustomList<T> CopyList(CustomList<T> originalList)
+        {
+            CustomList<T> copyList = new CustomList<T>();
+
+            foreach (T element in originalList)
+            {
+                copyList.Add(element);
+            }
+            return copyList;
+        }
+        public static CustomList<T> operator -(CustomList<T> listOne, CustomList<T> listTwo)
+        {
+            CustomList<T> temporaryList = CopyList(listOne);
+
+            foreach (T itemOne in listOne)
+            {
+                foreach (T itemTwo in listTwo)
+                {
+                    if (itemOne.Equals(itemTwo))
+                    {
+                        temporaryList.Remove(itemOne);
+                    }
+                }
+            }
+            return temporaryList;
+        }
+        public CustomList<T> Zipper(CustomList<T> listOne, CustomList<T> listTwo){
+            CustomList<T> zipperedList = new CustomList<T>();
+            CustomList<T> biggerList = new CustomList<T>();
+            CustomList<T> smallerList = new CustomList<T>();
+            if (listOne.Count > listTwo.Count)
+            {
+                smallerList = listOne;
+                biggerList = listTwo;              
+            }
+            else {
+                smallerList = listTwo;
+                biggerList = listOne;
+            }
+            var temporaryValue = smallerList.GetEnumerator();
+            try
+            {
+                foreach (T itemInBiggerList in biggerList)
+                {
+                    zipperedList.Add(itemInBiggerList);
+                    if (temporaryValue.MoveNext()) { zipperedList.Add(temporaryValue.Current); }
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message);  }
+            return zipperedList;
+        }
+        public T ElementAt(int index)
+        {
+            return items[index];
+        }
+        public void Sort() 
+        {
+            int j;
+            try
+            {
+                for (int i = 0; i < manyItems; i++)
+                {
+                    j = i - 1;
+                    while (j >= 0 && items[j].CompareTo(items[j + 1]) > 0)
+                    {
+                        Swap(items, j);
+                        j = j - 1;
+                    }
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message);  }
+        }  
+        public void Swap(T[] array, int index) {
+            T temporaryVariable = default(T);
+            try
+            {
+                temporaryVariable = items[index + 1];
+                items[index + 1] = items[index];
+                items[index] = temporaryVariable;
+            }         
+            catch (Exception e) { Console.WriteLine(e.Message);  }
+}
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> myList = new CustomList<int>();
+            CustomList<int> myOtherList = new CustomList<int>();
+            myList.Add(8);
+            myList.Add(7);
+            myList.Add(3);
+            myList.Add(5);
+            myList.Add(4);
+            myList.Add(6);
+            myList.Add(2);
+            myList.Add(1);
+            myList.Remove(8);
+            myList.Remove(1);
+            myList.Remove(3);
+
+            myOtherList.Add(1);
+            myOtherList.Add(2);
+            myOtherList.Add(3);
+            myOtherList.Add(4);
+            myOtherList.Add(5);
+            myOtherList.Add(6);
+            myOtherList.Add(7);
+            myOtherList.Add(8);
+            myOtherList.Remove(1);
+            myOtherList.Remove(8);
+            myOtherList.Remove(4);
+            //Console.WriteLine(myOtherList);
+            //myList.ToString();
+            //foreach (object o in myList)
+            //{
+            //    Console.WriteLine(o);
+            //}
+            //CustomList<int> addTwoList = myList + myOtherList;
+            //Console.WriteLine(addTwoList);
+            //CustomList<int> subtractCopiesFromList = myList - myOtherList;
+            //Console.WriteLine(subtractCopiesFromList);
+            //Console.WriteLine(myList);
+            //Console.WriteLine("Zipper Output: {0}", myList.Zipper(myList, myOtherList));
+            //Console.WriteLine("First {0}", myList);
+            //myList.Sort();
+            //Console.WriteLine("Second {0}", myList);
+            //Console.WriteLine("Testing Sort {0}", myList);
+
+
+
+            Console.Read();
+        }
+    }
+}
